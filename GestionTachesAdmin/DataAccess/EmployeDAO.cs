c889efc693f48c2491cbc185a107ee3f57aa56b6
jason2 <jason2@JASONDESKTOP>
2026-07-29 17:25:12 +0300

    creation de model employe et employeDAO

---
+++ GestionTachesAdmin/DataAccess/EmployeDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using GestionTachesAdmin.Models;
+
+namespace GestionTachesAdmin.DataAccess
+{
+    public class EmployeDAO
+    {
+        public bool CreerEmploye(Employe emp)
+        {
+            using (var connexion = ConnexionBD.GetConnection())
+            {
+                string requete = "INSERT INTO EMPLOYE(matricule,nom,prenom,poste)" + " VALUES(@matricule,@nom,@prenom,@poste)";
+
+
+                using (var command = new MySqlCommand(requete, connexion))
+                {
+                    command.Parameters.AddWithValue("@matricule", emp.Matricule);
+                    command.Parameters.AddWithValue("@nom", emp.nom);
+                    command.Parameters.AddWithValue("@prenom", emp.prenom);
+                    command.Parameters.AddWithValue("@poste", emp.poste);
+
+                    connexion.Open();
+                    int ligneaffectes = command.ExecuteNonQuery();
+                    return ligneaffectes > 0;
+                }
+            }
+        }
+    }
+}
---
+++ GestionTachesAdmin/Models/Employe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTachesAdmin.Models
+{
+    public class Employe
+    {
+        public string Matricule {  get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string poste { get; set; }
+
+    }
+}
