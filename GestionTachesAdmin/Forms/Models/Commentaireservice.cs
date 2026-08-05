1787125676d1ddae482e99ffa73167be7c5cdafb
volaesperencia-hub <volaesperencia@gmail.com>
2026-08-05 03:38:27 +0300

    Ajout des fichiers Commentaireservice.cs    et     Rapportservice.cs

---
+++ GestionTachesAdmin/Forms/Models/Commentaireservice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class CommentaireService
+{
+    private readonly List<Commentaire> _commentaires = new();
+
+    public void AjouterCommentaire(int tacheId, int auteurId, string texte)
+    {
+        if (string.IsNullOrWhiteSpace(texte))
+            throw new ArgumentException("Le commentaire ne peut pas être vide.");
+
+        var nouveauCommentaire = new Commentaire
+        {
+            Id = _commentaires.Count + 1,
+            TacheId = tacheId,
+            AuteurId = auteurId,
+            Contenu = texte,
+            DatePublication = DateTime.Now
+        };
+
+        _commentaires.Add(nouveauCommentaire);
+    }
+
+    public List<Commentaire> ObtenirCommentairesParTache(int tacheId)
+    {
+        return _commentaires
+            .Where(c => c.TacheId == tacheId)
+            .OrderByDescending(c => c.DatePublication)
+            .ToList();
+    }
+}
---
+++ GestionTachesAdmin/Forms/Models/Rapportservices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+public class ProductiviteService
+{ 
+    public RapportProductivite GenererRapport(int employeId, List<Tache> toutesLesTaches, DateTime debut, DateTime fin)
+    {
+        var tachesEmploye = toutesLesTaches
+            .Where(t => t.EmployeId == employeId && t.DateCreation >= debut && t.DateCreation <= fin)
+            .ToList();
+
+        int terminees = tachesEmploye.Count(t => t.Statut == StatutTache.Terminee);
+        int enRetard = tachesEmploye.Count(t => t.Statut != StatutTache.Terminee && t.DateEcheance < DateTime.Now);
+
+        return new RapportProductivite
+        {
+            EmployeId = employeId,
+            DateDebutPeriode = debut,
+            DateFinPeriode = fin,
+            TachesAssignees = tachesEmploye.Count,
+            TachesTerminees = terminees,
+            TachesEnRetard = enRetard
+        };
+    }
+}
